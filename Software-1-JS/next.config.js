be83821f8bb0c3db229374bff6e2f7df343d6a47
Josh Bowen <jbowen@nebraska.edu>
2023-10-02 12:35:36 -0500

    Update base path

--- Software-1-JS/next.config.js
+++ Software-1-JS/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/8000-Tech-Studio/Software-1-JS/out",
+    images: { unoptimized: true },
     output: 'export',
+    trailingSlash: true,
     webpack(config, { isServer }) {
         const prefix = config.assetPrefix ?? config.basePath ?? '';
         config.module.rules.push({
